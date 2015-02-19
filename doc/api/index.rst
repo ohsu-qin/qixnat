eddde10376e00aa9c80bc3e1e8ce126b14a825cc
FredLoney <loneyf@ohsu.edu>
2015-02-18 16:28:11 -0800

    Standardize the autodoc directives.

--- doc/api/index.rst
+++ doc/api/index.rst
@@ -7,35 +7,23 @@ API Documentation
 :mod:`command`
 --------------
 .. automodule:: qixnat.command
-    :members:
-    :show-inheritance:
 
 :mod:`configuration`
 --------------------
 .. automodule:: qixnat.configuration
-    :members:
-    :show-inheritance:
 
 :mod:`connection`
 -----------------
 .. automodule:: qixnat.connection
-    :members:
-    :show-inheritance:
 
 :mod:`constants`
 -----------------
 .. automodule:: qixnat.constants
-    :members:
-    :show-inheritance:
 
 :mod:`facade`
 -------------
 .. automodule:: qixnat.facade
-    :members:
-    :show-inheritance:
 
 :mod:`helpers`
 --------------
 .. automodule:: qixnat.helpers
-    :members:
-    :show-inheritance:
--- doc/conf.py
+++ doc/conf.py
@@ -3,6 +3,7 @@ import qixnat
 
 extensions = ['sphinx.ext.autodoc', 'sphinx.ext.intersphinx', 'sphinx.ext.todo']
 autoclass_content = "both"
+autodoc_default_flags= ['members', 'show-inheritance']
 source_suffix = '.rst'
 master_doc = 'index'
 project = u'qixnat'
@@ -11,3 +12,17 @@ version = qixnat.__version__
 pygments_style = 'sphinx'
 htmlhelp_basename = 'qxnatdoc'
 html_title = "qixnat v%s" % version
+
+
+def skip(app, what, name, obj, skip, options):
+    """
+    @return False if the name is __init__ or *skip* is set, True otherwise
+    """
+    return skip and name is not "__init__"
+
+
+def setup(app):
+    """
+    Directs autodoc to call :meth:`skip` to determine whether to skip a member.
+    """
+    app.connect("autodoc-skip-member", skip)
